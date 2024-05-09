b14f647b8d87269fd8fd1d5e7a7fb11857f953f0
dery <aryputragamers@gmail.com>
2024-05-09 14:55:02 +0700

    im not sure what i made here

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,35 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    darkMode: "class", // or 'media'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "brand-blue": "#1992d4",
+                "brand-red": "#e3342f",
+                "navbar-gray": "#f5f5f5", // Custom color for the navbar
+            },
+            spacing: {
+                128: "32rem",
+                144: "36rem",
+                "navbar-padding": "1rem", // Custom padding for the navbar
+            },
+            fontFamily: {
+                sans: ["Graphik", "sans-serif"],
+                serif: ["Merriweather", "serif"],
+            },
+        },
+    },
+    variants: {
+        extend: {
+            borderColor: ["focus-visible"],
+            opacity: ["disabled"],
+        },
     },
     plugins: [],
-};
-module.exports = {
-    extend: {
-        // Extend or add custom colors, spacing, etc., specifically for the navbar
-        colors: {
-            "navbar-gray": "#f5f5f5", // Custom color
-        },
-        spacing: {
-            "navbar-padding": "1rem", // Custom spacing
-        },
-    },
 };
